2db446559bb9c11d755300c15f4480659a356999
periplox <periplox@gmail.com>
2025-02-19 14:01:11 -0300

    Add extraFields to formData

--- src/js/egodesign.form.js
+++ src/js/egodesign.form.js
@@ -200,8 +200,11 @@ export default class EgoForm {
         }
 
         if (this.extraFields.length) {
+            console.log('HAS EXTRA FIELDS');
+
             for (const field of this.extraFields) {
                 if (field.hasOwnProperty('name') && field.hasOwnProperty('value')) {
+                    formData.append(field.name, field.value);
                     jsonData[field.name] = field.value;
                 }
             }
